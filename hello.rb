334b66f6663ef28d2ec8b7be8dc0730c961dcfa4
Yoshihide Chubachi <yoshi@chubachi.net>
2016-09-19 14:07:37 +0900

    HTML tag

--- hello.rb
+++ hello.rb
@@ -1,5 +1,16 @@
+# coding: utf-8
 require 'sinatra'
 
 get '/' do
-  '<H1>Hello world, AIIT</H1>'
+<<EOS
+<html>
+  <head>
+    <title>AIIT Hello</title>
+  </head>
+  <body>
+    <H1>Hello world, AIIT</H1>
+    こんにちは，これはHerokuで動作するWebアプリです．
+  </body>
+</html>
+EOS
 end
